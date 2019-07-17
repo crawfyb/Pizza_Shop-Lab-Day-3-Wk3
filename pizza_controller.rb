82f761fed705aa16d5301398d74e903c2188d035
crawfyb <crawford.brown@outlook.com>
2019-07-17 13:51:00 +0100

    first commit

---
+++ pizza_controller.rb
@@ -0,0 +1,37 @@
+require( 'sinatra' )
+require( 'sinatra/reloader' )
+require( 'pry-byebug' )
+
+require_relative('./models/pizza_order.rb')
+also_reload('./models/*')
+
+# index
+get '/pizza-orders' do
+  @orders =PizzaOrder.all()
+  erb(:index)
+end
+
+# new
+get '/pizza-orders/new' do
+  erb(:new)
+end
+
+# show
+get '/pizza-orders/:id' do
+  @order = PizzaOrder.find(params[:id])
+  erb(:show)
+end
+
+# create
+post '/pizza-orders' do
+  @order = PizzaOrder.new(params)
+  @order.save()
+  erb(:create)
+end
+
+# delete
+
+post '/pizza-orders/:id/delete' do
+  @order = PizzaOrder.delete(params[:id])
+  redirect('/pizza-orders')
+end
